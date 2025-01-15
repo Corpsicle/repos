0745666174493cc6568c7e89d010d47bc0415994
Alan Burr <10787154+Corpsicle@users.noreply.github.com>
2025-01-15 17:14:25 +0000

    added title & Column Headings to Fahrenheit.c

--- VC/Fahrenheit/Fahrenheit/Fahrenheit.c
+++ VC/Fahrenheit/Fahrenheit/Fahrenheit.c
@@ -13,11 +13,13 @@ int main()
 	lower = 0;      /* lower limit of temperature scale */
 	upper = 300;    /* upper limit */
 	step = 20;      /* step size */
-	
+
+	printf("\n\nProgram: Fahrenheit to Celsius Table\n\n");
+	printf("Fahrenheit\tCelsius\n");
 	fahr = lower;
 	while (fahr <= upper) {
 		celsius = (5.0/9.0) * (fahr - 32.0);
-		printf("%3.0f\t%6.1f\n", fahr, celsius);
+		printf("%6.0f\t\t%6.1f\n", fahr, celsius);
 		fahr = fahr + step;
 	}
 	return 0;
