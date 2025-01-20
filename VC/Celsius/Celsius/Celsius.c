00431c88594e409de0075facbca214f4e770690d
Alan Burr <10787154+Corpsicle@users.noreply.github.com>
2025-01-20 08:42:28 +0000

    Helloworld added to c++ folder

---
+++ VC/Celsius/Celsius/Celsius.c
@@ -0,0 +1,38 @@
+// Celsius.c : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <stdio.h>
+
+/* print Celsius-Fahrenheit table
+	for celsius = 0, 20, ..., 300; floating point version */
+
+int main()
+{
+	float fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0;		/* lower limit of temperature table */
+	upper = 300;	/* upper limit */
+	step = 20;		/* step size */
+
+	printf("Celsius\tFahrenheit\n");
+	celsius = lower;
+	while (celsius <= upper) {
+		fahr = ((celsius / 5.0) * 9.0) + 32.0;
+		printf("%3.0f\t%6.1f\n", celsius, fahr);
+		celsius = celsius + step;
+	}
+	return 0;
+}
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- VC/Fahrenheit/Fahrenheit/Fahrenheit.c
+++ VC/Fahrenheit/Fahrenheit/Fahrenheit.c
@@ -1,5 +1,5 @@
-// Fahrenheit.c : This file contains the 'main' function. Program execution begins and ends there.
-//
+/* Fahrenheit.c : This file contains the 'main' function.Program execution begins and ends there.
+*/
 
 #include <stdio.h>
 
@@ -14,7 +14,7 @@ int main()
 	upper = 300;    /* upper limit */
 	step = 20;      /* step size */
 
-	printf("\n\nProgram: Fahrenheit to Celsius Table\n\n");
+	printf("\n\nProgram: to print Fahrenheit to Celsius Table\n\n");
 	printf("Fahrenheit\tCelsius\n");
 	fahr = lower;
 	while (fahr <= upper) {
@@ -26,13 +26,13 @@ int main()
      
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+/* Run program : Ctrl + F5 or Debug > Start Without Debugging menu
+   Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+   Tips for Getting Started: 
+     1. Use the Solution Explorer window to add/manage files
+     2. Use the Team Explorer window to connect to source control
+     3. Use the Output window to see build output and other messages
+     4. Use the Error List window to view errors
+     5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+     6. In the future, to open this project again, go to File > Open > Project and select the .sln file */
