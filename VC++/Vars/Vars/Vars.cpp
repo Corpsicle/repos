6d7133a4fa3fb1448b873db2257debb17f68db89
Alan Burr <10787154+Corpsicle@users.noreply.github.com>
2025-01-20 09:01:58 +0000

    added Vars to C++

---
+++ VC++/Vars/Vars/Vars.cpp
@@ -0,0 +1,33 @@
+// Vars.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char letter; letter = 'A';	// Declared then initialized
+	int number; number = 100;	// Declared then initialized
+	float decimal = 7.5;		// Declared then initialized
+	double pi = 3.14159;		// Declared then initialized
+	bool isTrue = true;			// Declared then initialized
+
+	cout << "The letter is: " << letter << endl;
+	cout << "The number is: " << number << endl;
+	cout << "The decimal is: " << decimal << endl;
+	cout << "The value of pi is: " << pi << endl;
+	cout << "The statement is: " << isTrue << endl;
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
