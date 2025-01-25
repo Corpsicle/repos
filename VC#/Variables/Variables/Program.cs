1071933ab852a16ebf33142d872841231b08a103
Alan Burr <10787154+Corpsicle@users.noreply.github.com>
2025-01-25 18:58:01 +0000

    Added to C# folder

---
+++ VC#/Variables/Variables/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Title = "Variables";
+
+char letter ;  letter = 'A';    // Declared then initialied.
+int number;    number = 100;    // Declared then initialied.
+float body = 98.6f;             // Declared and initialied.
+double pi = 3.14159;            // Declared and initialied.
+decimal sum = 1000.00m;         // Declared and initialied.
+bool flag = false;              // Declared and initialied.
+string text = "C# is fun!";     // Declared and initialied. 
+
+Console.WriteLine("char Letter:\t " + letter);
+Console.WriteLine("int Number:\t " + number);
+Console.WriteLine("float Body:\t " + body);
+Console.WriteLine("double Pi:\t " + pi);
+Console.WriteLine("decimal Sum:\t " + sum);
+Console.WriteLine("bool Flag:\t " + flag);
+Console.WriteLine("string Text:\t " + text);
---
+++ VC#/input/input/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Input";
+        Console.Write("Please Enter Yuor Name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Welcome " + name + "!");
+    }
+}
