96960c0f9f492ef3c1d4be0ef8287ae57a1bd191
Alan Burr <10787154+Corpsicle@users.noreply.github.com>
2025-01-28 17:30:53 +0000

    Added to VC# folder

---
+++ VC#/Cast/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Title = "Cast";
+
+double num = 10.5;
+int integer = 2;
+
+num = num + integer;
+Console.WriteLine( "Implicite Cast: " + num);
+num = (double) 7 / integer;
+Console.WriteLine("Explicite Cast: " + num);
+char letter = (char)65;
+Console.WriteLine("Cast integer: " + letter);
+int ascii = (int) 'A';
+Console.WriteLine("Cast char: " + ascii);
---
+++ VC#/Constant/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    enum Days { Sun, Mon, Tue, Wed, Thu, Fri, Sat };
+    private static void Main(string[] args)
+    {
+        const double pi = 3.14159265358979;
+        var daysType = typeof(Days);
+        Console.WriteLine( "Pi Type: " + pi.GetType() );
+        Console.WriteLine( "Circumference: " + (pi * 3) );
+        Console.WriteLine( "\nFirst Name: " + Days.Sat );
+        Console.WriteLine( "T1st Index: " + (int) Days.Sat );
+        string name = Enum.GetName( daysType, 1 );
+        Console.WriteLine("\n2nd Index: " + name );
+        bool flag = Enum.IsDefined(daysType, "Mon" );
+        Console.WriteLine("Contains Mon?: " + flag );
+        }
+}
