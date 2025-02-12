d7204a7780b3cfd929f9472a870aba4bd7af70e9
Alan Burr <10787154+Corpsicle@users.noreply.github.com>
2025-02-12 17:24:51 +0000

    Added to VC Folder

---
+++ VC/newfile/newfile.c
@@ -0,0 +1,34 @@
+/* newfile.c : This file contains the 'main' function.Program execution begins and ends there.  */
+
+#include <stdio.h>
+
+
+int main()
+{
+    FILE* file_ptr;
+    errno_t err = fopen_s(&file_ptr,"data.txt", "w");
+    if (err  == 0)
+    {
+        printf("File created\n");
+		fclose(file_ptr);
+        return 0;
+    }
+    else
+    {
+		printf("Unable to create file\n");
+		return 1;
+    }
+    return 0;
+}
+
+/* Run program : Ctrl + F5 or Debug > Start Without Debugging menu
+   Debug program: F5 or Debug > Start Debugging menu  */
+
+/* Tips for Getting Started :
+     1. Use the Solution Explorer window to add/manage files
+     2. Use the Team Explorer window to connect to source control
+     3. Use the Output window to see build output and other messages
+     4. Use the Error List window to view errors
+     5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+     6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+*/
