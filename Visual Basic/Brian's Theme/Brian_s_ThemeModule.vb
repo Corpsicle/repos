270671595c187883dcef42d7ab4ccb24235caa9b
Alan Burr <10787154+Corpsicle@users.noreply.github.com>
2025-04-16 12:14:10 +0100

    Changes to Brian's Theme

--- Visual Basic/Brian's Theme/Brian_s_ThemeModule.vb
+++ Visual Basic/Brian's Theme/Brian_s_ThemeModule.vb
@@ -1,52 +1,52 @@
 Friend Module Brian_s_ThemeModule
-    Dim PenColour, BackgroundColour, windowWidth, windowHeight, CenterX, CenterY, Stp, I, S As Primitive
+    Dim windowWidth, windowHeight, PenColour, BackgroundColour, CenterX, CenterY, Stp, I, S As Primitive
     Sub Main()
         ' Brian's Theme.sb
-
-        PenColour = "White"
-        BackgroundColour = "Black"
-
-        GraphicsWindow.Width = 640
-        GraphicsWindow.Height = 480
-        GraphicsWindow.Title = "Brian's Theme"
-        GraphicsWindow.BackgroundColor = BackgroundColour
-        GraphicsWindow.PenColor = PenColour
+        ' Initial setup of window 
+        GraphicsWindow.Width = 1024 ' Size of Window we are using  1024 x 768 pixels 
+        GraphicsWindow.Height = 768
+        GraphicsWindow.Title = "Brian's Theme" ' Initial window Title
         GraphicsWindow.Show()
-
-        windowWidth = GraphicsWindow.Width      ' Width 
-        windowHeight = GraphicsWindow.Height    ' and Height of window in Pixels
-
+        windowWidth = GraphicsWindow.Width ' Width 
+        windowHeight = GraphicsWindow.Height ' and Height of window in Pixels
 
 Start:
+        PenColour = GraphicsWindow.GetRandomColor() ' Get a Random Pen coloyr  to draw in
+        BackgroundColour = GraphicsWindow.GetRandomColor() ' Get a randome Background colour to draw on 
+        If PenColour = BackgroundColour Then ' let's make sure we aren't drawing in the same 
+            GoTo Start ' colour as the backgound 
+        End If
+        GraphicsWindow.BackgroundColor = BackgroundColour ' all ok then set the backround colour
         GraphicsWindow.Clear()
-        CenterX = Microsoft.SmallBasic.Library.Math.GetRandomNumber(windowWidth)    ' Get a Random  X coordinate for center
-        CenterY = Microsoft.SmallBasic.Library.Math.GetRandomNumber(windowHeight)   ' and the same for a Y coordinate
-        Stp = Microsoft.SmallBasic.Library.Math.GetRandomNumber(5) + 3              ' draw a line every Stp pixels apart
+        CenterX = Microsoft.SmallBasic.Library.Math.GetRandomNumber(windowWidth) ' Get a Random  X coordinate for center
+        CenterY = Microsoft.SmallBasic.Library.Math.GetRandomNumber(windowHeight) ' and the same for a Y coordinate
+        Stp = Microsoft.SmallBasic.Library.Math.GetRandomNumber(5) + 3 ' draw a line every Stp pixels apart
 
+        GraphicsWindow.Title = "Brian's Theme        Stepping By : " + Stp + "          PenColour = " + PenColour + "    Backgound Colour = " + BackgroundColour
         ' Now lets draw lines first from top to bottom through the Center point  
         ' Top to Bottom
         For I = 0 To windowWidth Step Stp
             For S = 0 To 1
                 GraphicsWindow.DrawLine(I, 1, CenterX, CenterY)
-                GraphicsWindow.PenColor = "Black"
+                GraphicsWindow.PenColor = BackgroundColour
                 GraphicsWindow.DrawLine(I + S, 1, CenterX, CenterY)
                 GraphicsWindow.PenColor = PenColour
                 GraphicsWindow.DrawLine(CenterX, CenterY, windowWidth - I, windowHeight)
-                GraphicsWindow.PenColor = "Black"
+                GraphicsWindow.PenColor = BackgroundColour
                 GraphicsWindow.DrawLine(CenterX, CenterY, windowWidth - I - S, windowHeight)
                 GraphicsWindow.PenColor = PenColour
             Next
         Next
 
         ' and again this time from  Side to side through the center point 
-        For I = windowHeight To 0 Step -Stp
+        For I = 0 To windowHeight Step Stp
             For S = 0 To 1
-                GraphicsWindow.DrawLine(windowWidth, windowHeight - I, CenterX, CenterY)
-                GraphicsWindow.PenColor = "Black"
-                GraphicsWindow.DrawLine(windowWidth, windowHeight - I - S, CenterX, CenterY)
+                GraphicsWindow.DrawLine(windowWidth, I, CenterX, CenterY)
+                GraphicsWindow.PenColor = BackgroundColour
+                GraphicsWindow.DrawLine(windowWidth, I + S, CenterX, CenterY)
                 GraphicsWindow.PenColor = PenColour
                 GraphicsWindow.DrawLine(CenterX, CenterY, 1, windowHeight - I)
-                GraphicsWindow.PenColor = "Black"
+                GraphicsWindow.PenColor = BackgroundColour
                 GraphicsWindow.DrawLine(CenterX, CenterY, 1, windowHeight - I - S)
                 GraphicsWindow.PenColor = PenColour
             Next
