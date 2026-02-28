f3aebf2358d07f0479f0a9192ff85e1dbb411cb4
alanb <alanb@localhost>
2026-02-28 08:25:07 +0000

    Update C/C++ settings and regenerate assembly info for C# projects

--- C#/Variables/obj/Release/net10.0/Variables.AssemblyInfo.cs
+++ C#/Variables/obj/Release/net10.0/Variables.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Variables")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+458e10d8b3594a234189b564c3e450e2b7acf482")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e92372dbfe130d0aa1d55b075334154266e3ce45")]
 [assembly: System.Reflection.AssemblyProductAttribute("Variables")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Variables")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
