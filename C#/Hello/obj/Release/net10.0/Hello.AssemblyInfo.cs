88765f828aec8e78268cb0aab5cb7773c695895a
alanb <alanb@localhost>
2026-03-05 07:49:20 +0000

    Update assembly informational version and cache files for Hello and Variables projects

--- C#/Hello/obj/Release/net10.0/Hello.AssemblyInfo.cs
+++ C#/Hello/obj/Release/net10.0/Hello.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hello")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17364953ccbdfc7a725be543c2a6e52743aa0d76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb46d903b8d5b7f01bc357618ba58da894449801")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hello")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hello")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
