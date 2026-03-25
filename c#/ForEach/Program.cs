f5538c639a0787158e6582112eaccf6339bccb88
alanb <alanb@localhost>
2026-03-25 14:14:55 +0000

    added ForEach to C#

---
+++ c#/ForEach/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Title = "ForEach";
+
+string [] websites = new string [5] {
+    "Google",
+    "Facebook",
+    "Twitter",
+    "LinkedIn",
+    "Instagram"
+};
+
+Console.WriteLine( "Popular Websites..." );
+int rank = 1;
+foreach ( string site in websites )
+{
+    Console.WriteLine( $"Position: {rank}.\t{site}" );
+    rank++;
+}
+Console.ReadKey();
