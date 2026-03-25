a8dbd15a9d869ffb5130a7e661e07e6bb446a700
alanb <alanb@localhost>
2026-03-25 15:36:19 +0000

    added KeyValue.cs

---
+++ c#/KeyValue/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Title = "KeyValue";
+
+Dictionary< string, string > BookList = 
+        new Dictionary < string, string > ();
+BookList.Add( "Stuart Yarnold", "Arduino" );
+BookList.Add( "Nick Vandome", "Windows 10" );
+BookList.Add( "Mike McGrath", "Java" );
+Console.WriteLine( "Popular Titles..." );
+foreach ( KeyValuePair < string, string > book in BookList )
+{
+    Console.WriteLine( "Key: " + book.Key + "\tValue: " + book.Value + " in easy steps" );
+}
+Console.ReadLine();
