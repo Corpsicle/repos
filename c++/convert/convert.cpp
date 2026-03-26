f5072bc156870916497efc347235f81fa12f6faa
alanb <alanb@localhost>
2026-03-26 17:30:27 +0000

    added input and convert too c++ folder

---
+++ c++/convert/convert.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <sstream>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string term = "100";
+    int number = 100;
+    int num;                // To store a converted string
+    string text;            // To store a converted integer
+    stringstream stream;    // T perform conversions
+
+    stream << term;         // Load the string
+    stream >> num;          // Extract the integer
+    num /= 4;
+    cout << "Integer value: " << num << endl;
+    stream.str("");         // Empty the contents
+    stream.clear();         // Empty the bit flags.
+    stream << number;      // Load the integer.
+    stream >> text;        // Extract the string.
+    text += " Per Cent";
+    cout << "String value: " << text << endl;
+    
+    return 0;
+}
---
+++ c++/input/input.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    cout << "Please enter your full name: ";
+    cin >> name;
+    cout << "Welcome, " << name << endl;
+    cout << "Please enter your full name: ";
+    cin.ignore( 256, '\n' );
+    getline(cin, name);
+    cout << "Thanks, " << name << endl;
+
+    return 0;
+}
