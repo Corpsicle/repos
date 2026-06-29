d291e550825540ebe617dc2ad71c36fc3a360edd
alanb <alanb@localhost>
2026-06-29 10:12:34 +0100

    Update AssemblyInfo files and cache with new informational version for multiple projects

--- c#/Input/obj/Release/net10.0/Input.AssemblyInfo.cs
+++ c#/Input/obj/Release/net10.0/Input.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Input")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4ac45179fa4d9c0702928e6c646d45236b5a916")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea9cf5a99b429a40fb955b6a77c1c2c0bc088850")]
 [assembly: System.Reflection.AssemblyProductAttribute("Input")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Input")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
